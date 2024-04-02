5f203598ec3f8916b719b6ea56bd03943ad68f78
jovik31 <joaogvicente@gmail.com>
2024-04-02 22:29:29 +0100

    Added iptables logic for docker presence and isolation between tenants on the same host

--- pkg/network/routing/iptables.go
+++ pkg/network/routing/iptables.go
@@ -3,6 +3,7 @@ package routing
 import (
 	"github.com/pkg/errors"
 	"os/exec"
+	"github.com/coreos/go-iptables/iptables"
 
 	//"github.com/coreos/go-iptables/iptables"
 )
@@ -31,5 +32,45 @@ func EnableIPForwarding() error {
 	if err := cmd.Run(); err != nil {
 	return errors.Wrapf(err,"Failed to enable IP forwarding")
 }
+	return nil
+}
+
+func AddIptablesBridge(bridgeName, hostDeviceName, nodeCIDR string) error {
+	ipt, err := iptables.NewWithProtocol(iptables.ProtocolIPv4)
+	if err != nil {
+		return err
+	}
+
+	if err := ipt.AppendUnique("filter", "FORWARD", "-i", bridgeName, "-j", "ACCEPT"); err != nil {
+		return err
+	}
+	return nil
+}
+
+func AddIptablesHost(hostDeviceName, nodeCIDR string, clusterCIDR string) error {
+	ipt, err := iptables.NewWithProtocol(iptables.ProtocolIPv4)
+	if err != nil {
+		return err
+	}
+	if err := ipt.AppendUnique("filter", "FORWARD", "-i", hostDeviceName, "-j", "ACCEPT"); err != nil {
+		return err
+	}
+	if err := ipt.AppendUnique("nat", "POSTROUTING", "-s", clusterCIDR, "-d", clusterCIDR,"-j", "MASQUERADE"); err != nil {
+		return err
+	}
+
+	return nil
+}
+//Check if this "iptables -P FORWARD ACCEPT" is needed
+
+func IsolateTenant(tenant1CIDR string, tenant2CIDR string) error {
+	ipt, err := iptables.NewWithProtocol(iptables.ProtocolIPv4)
+	if err != nil {
+		return err
+	}
+	//Check if FORWARD is the correct chain to add this rule
+	if err := ipt.AppendUnique("filter", "FORWARD", "-s", tenant1CIDR,"-d", tenant2CIDR, "-j", "DROP"); err != nil {
+		return err
+	}
 	return nil
 }
